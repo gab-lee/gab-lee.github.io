15b0d3f8ae9b18fca472e5452fb0aeaf3214fab0
Gabriel <53164692+Gabldn@users.noreply.github.com>
2023-06-05 16:03:10 +0800

    Pivot to React portfolio

---
+++ MyPortfolio/src/Message.tsx
@@ -0,0 +1,8 @@
+// use function based components for react 
+//Use pascal casing and Javascript XML - JSX
+function Message(){
+    return <h1>Hi my name is Gabriel</h1>
+
+}
+
+export default Message;
